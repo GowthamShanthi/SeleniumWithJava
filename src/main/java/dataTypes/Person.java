710dce0bfd4134aff8cbe2fb7a4fee75db48dfb4
GowthamShanthi <gowtham.shanthi@gmail.com>
2024-06-23 09:53:48 +0530

    Modifying scripts

---
+++ src/main/java/dataTypes/Person.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Person {
+	
+public static void main(String[] args) {
+	
+String name = "Gowtham";
+System.out.println("My name is"+name);
+byte age = 34;
+float weight = 65.5f;
+double height = 165.789797897;
+long phonenumber = 1234567890;
+int number = 83522;
+short id = 2222;
+char initial = 'V';
+boolean liked = true;
+
+	
+}
+
+}
---
+++ src/main/java/namachivayaVaalga/Namachivaya.java
@@ -0,0 +1,12 @@
+package namachivayaVaalga;
+
+public class Namachivaya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("NamachivayaVaalga");
+
+	}
+
+}
