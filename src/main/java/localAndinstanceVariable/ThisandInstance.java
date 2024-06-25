1aa516c94548e0af0507f22c165c8f6376f708ed
GowthamShanthi <gowtham.shanthi@gmail.com>
2024-06-25 09:32:33 +0530

    Adding Local variable and instance variable files

---
+++ src/main/java/localAndinstanceVariable/ThisandInstance.java
@@ -0,0 +1,32 @@
+package localAndinstanceVariable;
+
+public class ThisandInstance {
+	
+int b = 50;
+
+public static void sum() {
+	
+	int a = 10;
+	int b = 20;
+	
+	System.out.println("The value of a is"+a);
+	System.out.println("The value of local variable b is"+b);
+	System.out.println("The value of instance variable b is"+this.b);
+	
+	
+	
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ThisandInstance th = new ThisandInstance();
+		 th.sum();
+		 
+		 System.out.println("Tryingt to call instance variable by this"+b);
+
+	}
+
+}
